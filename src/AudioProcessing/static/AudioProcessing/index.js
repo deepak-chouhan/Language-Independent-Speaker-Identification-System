537e0cfd67a5296515993a35236a7efa422207b1
deepak <deepakceo0@gmail.com>
2022-04-11 09:28:04 +0530

    added logic to convert blob to base64 string

--- src/AudioProcessing/static/AudioProcessing/index.js
+++ src/AudioProcessing/static/AudioProcessing/index.js
@@ -1,11 +1,16 @@
 $(document).ready(() => {
 
-    async function doSomethingWithAudioBuffer(blob) {
-        var arrayBuffer = await blob.arrayBuffer();
-        // Do something with arrayBuffer;
-        console.log("doSomethingWithAudioBuffer()->")
-        console.log(arrayBuffer);
-      }
+    // converts the blob into base64 string
+    function getString(blob){
+        var reader = new FileReader();
+        reader.readAsDataURL(blob);
+        reader.onloadend = function () {
+            var base64data = reader.result;
+
+            // logic to send the base64 string to server
+            console.log(base64data);
+        }
+    }
 
     var device = navigator.mediaDevices.getUserMedia({
         audio: true
@@ -15,29 +20,22 @@ $(document).ready(() => {
         var recorder = new MediaRecorder(stream);
         recorder.ondataavailable = e => {
             items.push(e.data);
-            // console.log(e.data);
+            
             if (recorder.state == "inactive") {
                 var blob = new Blob(items, {
                     type: "audio/webm"
                 });
+
+                // display the audio on frontend
                 var audio = document.getElementById("audio");
                 var mainaudio = document.createElement("audio");
                 mainaudio.setAttribute("controls", "controls");
                 audio.appendChild(mainaudio);
                 mainaudio.innerHTML = '<source src="' + URL.createObjectURL(blob) + '" type="video/webm"/>';
-
-                const audioContext = new AudioContext();
-                const fileReader = new FileReader();
-
-                fileReader.onloadend = () => {
-                    let myArrayBuffer = fileReader.result;
-                    audioContext.decodeAudioData(myArrayBuffer, (audioBuffer) => {
-                        console.log(audioBuffer)
-                    });
-                };
-
-                doSomethingWithAudioBuffer(blob);
-
+                // -----------------------------
+                
+                // convert the blob to base64
+                getString(blob);
             }
         }
 
