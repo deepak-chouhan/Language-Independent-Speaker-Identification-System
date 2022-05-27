9fff7ae1f10df8a45acc03f7555312c7a66029d2
deepak <deepakceo0@gmail.com>
2022-05-27 19:56:20 +0530

    dashboard

--- src/AudioProcessing/urls.py
+++ src/AudioProcessing/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
     path("dashboard", views.dashboard, name="dashboard"),
     path("login", views.my_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    
 ]
