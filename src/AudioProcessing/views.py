6acecec75b9722c07b6eeff4ca4368040ed5e723
deepak <deepakceo0@gmail.com>
2022-05-31 13:22:15 +0530

    added logic to export data as csv

--- src/AudioProcessing/views.py
+++ src/AudioProcessing/views.py
@@ -1,13 +1,14 @@
+import os
+from django.conf import settings
 from django.shortcuts import render,redirect
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from requests import patch
 from .forms import *
 import base64
 from django.contrib.auth import authenticate, login,logout
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 import csv
-
 from .helper import *
 
 def my_view(request):
@@ -41,21 +42,28 @@ def dashboard(request):
         batch = request.POST["batch"]
         date = request.POST["date"]
 
-        print(course,batch,date)
-        dates=Attendance.objects.filter(course=course.upper(),date=date, batch=batch.upper())
-        f = open("filename.csv", "w")
-        f.truncate()
-        writer = csv.writer(f)
-        head=[course.upper(),batch]
-        writer.writerow(head)
+        dates = Attendance.objects.filter(course=course, date__contains=date, batch=batch.upper())
+        print(dates)
+
+
+        file = open("filename.csv", "w")
+        file.truncate()
+        writer = csv.writer(file)
+
         head2=['Name','roll','date']
         writer.writerow(head2)
         for i in dates:
             date=[i.student_atnd.name, i.student_atnd.roll_no, i.date]
             print(date)
             writer.writerow(date)
-        f.close()
-            
+        file.close()
+
+        file_path = "filename.csv"
+        with open(file_path, 'rb') as fh:
+            response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+            response['Content-Disposition'] = 'inline; filename=' + file_path
+            return response
+
     return render(request, "AudioProcessing/dashboard.html")
 
 @csrf_exempt
